3a328204e0a3d4766f3d2f3f60965ae8defff48b
typhone-99 <typhoooone.99@gmail.com>
2025-02-04 22:03:15 +0900

    Update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,18 +10,15 @@ export default function Header() {
       const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
     return (
-        <div className="z-50 px-8 py-4 border-b h-16 border-slate-200 flex items-center bg-white/75 backdrop-blur-3xl sticky top-0">
+        <div className="z-50 px-8 py-4 h-16 flex items-center bg-white sticky top-0">
           <Link href="/" className="flex items-center select-none">
             <Image src="/logo.svg" alt="Logo" width={100} height={100} className="h-8 w-fit aspect-square" />
             <p className="text-xl ml-2 italic">Water UI</p>
           </Link>
-          <div className="ml-auto hidden md:flex items-center space-x-2">
-            <Link href="https://github.com/tyseries/ui" target="_blank" rel="noopener noreferrer" className="w-8 h-8 border border-slate-200 duration-200 hover:bg-slate-200 flex items-center justify-center rounded-full">
-              <FaGithub className="text-xl" />
-            </Link>
-            <Link href="https://github.com/tyseries/ui" target="_blank" rel="noopener noreferrer" className="h-8 px-4 text-sm border border-slate-200 duration-200 hover:bg-slate-200 flex items-center justify-center rounded-full">
-              Install via npm!
-            </Link>
+          <div className="ml-auto hidden md:flex items-center flex-wrap gap-2">
+          <Link href="https://github.com/tyseries/ui" target="_blank" rel="noopener noreferrer" className="w-8 h-8 aspect-square flex items-center justify-center border rounded-lg border-slate-200 hover:bg-slate-200 duration-200">
+            <FaGithub className="text-xl" />
+          </Link>
           </div>
           <div className="ml-auto md:hidden relative">
             <button onClick={toggleMenu} className="w-8 h-8 rounded-full flex items-center justify-center hover:bg-slate-200 duration-200 border border-slate-200">
@@ -39,11 +36,11 @@ export default function Header() {
                 <Input placeholder="Search docs..." className="my-4 shadow-sm w-full" size="sm" />
                 <div className="flex flex-col overflow-y-auto">
                   <p className="mb-2">Documentation</p>
-                  <Link href="/docs/introduction" className="text-sm px-4 py-2 rounded-full hover:bg-slate-200 duration-200 text-slate-600">Introduction</Link>
-                  <Link href="/docs/get-to-started" className="text-sm px-4 py-2 rounded-full hover:bg-slate-200 duration-200 text-slate-600">Get to Stared</Link>
+                  <Link href="/docs/introduction" className="outline-none text-sm px-4 py-2 rounded-full hover:bg-slate-200 duration-200 text-slate-600 focus-visible:bg-slate-200">Introduction</Link>
+                  <Link href="/docs/get-to-started" className="outline-none text-sm px-4 py-2 rounded-full hover:bg-slate-200 duration-200 text-slate-600 focus-visible:bg-slate-200">Get to Stared</Link>
                   <p className="mt-4 mb-2">Components</p>
-                  <Link href="/components/button" className="text-sm px-4 py-2 rounded-full hover:bg-slate-200 duration-200 text-slate-600">Button</Link>
-                  <Link href="/components/input" className="text-sm px-4 py-2 rounded-full hover:bg-slate-200 duration-200 text-slate-600">Input</Link>
+                  <Link href="/components/button" className="outline-none text-sm px-4 py-2 rounded-full hover:bg-slate-200 duration-200 text-slate-600 focus-visible:bg-slate-200">Button</Link>
+                  <Link href="/components/input" className="outline-none text-sm px-4 py-2 rounded-full hover:bg-slate-200 duration-200 text-slate-600 focus-visible:bg-slate-200">Input</Link>
                 </div>
                 <div className="mt-8 flex items-center flex-wrap gap-2 text-sm">
                   <Link href="https://github.com/tyseries/ui" target="_blank" rel="noopener noreferrer" className="px-2 py-1 border border-slate-200 duration-200 hover:bg-slate-200 flex items-center justify-center rounded-full">
